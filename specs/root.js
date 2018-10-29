755a64d10ae866d6f0576146333cfc2a043283e9
Iván Vicente Moreno Cencerrado <contacto@ivanmoreno.me>
2018-10-30 00:23:50 +0100

    Final commit

--- specs/root.js
+++ specs/root.js
@@ -1,39 +1,67 @@
 'use strict';
 
-// Endpoint /crear-nota/
-describe("Métodos", () => {
+describe("Métodos de la API", () => {
 
-    const Nota = {
-        a: 'a',
-        get validador() {
-            if (this.a != undefined && this.b != undefined) {
-                return true;
-            }
+    // Importamos el schema de las notas y los controladores
+    const Nota = require('../models/Nota');
+    const root = require('../controllers/root');
+
+    // Objetos request y response para testing
+    var req,
+        res;
+
+    beforeEach(() => {
+        // Reseteamos los objetos request y response
+        req = {
+            body: {},
+            query: {},
+            params: {}
         }
-    };
+        res = {
+            send: () => {},
+            sendStatus: () => {}
+        }
+    })
 
-    Nota.save = () => {
+    // Endpoint /crear-nota/
+    it('Crea una nueva nota en la base de datos', () => {
+        spyOn(Nota.prototype, 'save');
+        root.crearNota(req, res);
+        expect(Nota.prototype.save).toHaveBeenCalled();
+    });
 
-    };
+    // Endpoint /mis-notas/
+    it('Devuelve todas las notas almacenadas en la base de datos', () => {
+        spyOn(Nota, 'find');
+        root.listaNotas(req, res);
+        expect(Nota.find).toHaveBeenCalled();
+    });
 
-    describe('/crear-nota', () => {
-        const crearNota = () => {
-            const nuevaNota = new Nota({
-                titulo: req.body.titulo,
-                contenido: req.body.contenido
-            });
-            nuevaNota.save(err => {
-                if (err) {
-                    res.sendStatus(500);
-                }
-                res.sendStatus(200);
-            })
-        };
+    // Endpoint /nota/
+    it('Devuelve la primera nota cuyo título coincida con el regex', () => {
+        spyOn(Nota, 'find');
+        root.consultarNotaTitulo(req, res);
+        expect(Nota.find).toHaveBeenCalled();
+    });
+    it('Devuelve nota con el ID proporcionado', () => {
+        spyOn(Nota, 'findById');
+        // Preparamos el mock de la query
+        req.query.id = 3;
+        root.consultarNotaID(req, res);
+        expect(Nota.findById).toHaveBeenCalled();
+    });
 
-        it('Crea una nueva nota en la base de datos', () => {
-            spyOn(Nota, 'save');
-            crearNota();
-            expect(Nota.save).toHaveBeenCalled();
-        });
+    // Endpoint /marcar-favorita/
+    it('Permite marcar como favorita una nota', () => {
+        spyOn(Nota, 'findById');
+        root.marcarFavorita(req, res);
+        expect(Nota.findById).toHaveBeenCalled();
+    });
+
+    // Endpoint /favoritas/
+    it('Devuelve una lista de todas las notas marcadas como favoritas', () => {
+        spyOn(Nota, 'find');
+        root.listaFavoritas(req, res);
+        expect(Nota.find).toHaveBeenCalled();
     });
 })
