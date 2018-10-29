fad408727720cd16eca275f45ee9b05d9d740fdd
Iván Vicente Moreno Cencerrado <contacto@ivanmoreno.me>
2018-10-29 01:48:24 +0100

    Test specs

--- specs/root.js
+++ specs/root.js
@@ -1,3 +1,39 @@
-const root = require('../controllers/root');
+'use strict';
 
 // Endpoint /crear-nota/
+describe("Métodos", () => {
+
+    const Nota = {
+        a: 'a',
+        get validador() {
+            if (this.a != undefined && this.b != undefined) {
+                return true;
+            }
+        }
+    };
+
+    Nota.save = () => {
+
+    };
+
+    describe('/crear-nota', () => {
+        const crearNota = () => {
+            const nuevaNota = new Nota({
+                titulo: req.body.titulo,
+                contenido: req.body.contenido
+            });
+            nuevaNota.save(err => {
+                if (err) {
+                    res.sendStatus(500);
+                }
+                res.sendStatus(200);
+            })
+        };
+
+        it('Crea una nueva nota en la base de datos', () => {
+            spyOn(Nota, 'save');
+            crearNota();
+            expect(Nota.save).toHaveBeenCalled();
+        });
+    });
+})
